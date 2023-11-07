e36579813618a74d7822b6fe731afdbb1d967036
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-11-07 15:01:00 +0600

    returing variables hoisted in endpoint

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,8 @@ from fastapi.responses import JSONResponse
 from fastapi.responses import StreamingResponse
 import os
 
-from lib import generate_ast_and_get_json
+from AST import generate_ast_and_get_json
+from VariableHoisting import find_variables_to_test
 
 app = FastAPI()
 app.add_middleware(
@@ -24,21 +25,26 @@ class ASTRequest(BaseModel):
 @app.post("/api/v1/code/single")
 async def generate_AST_from_code_url(code_url: ASTRequest):
     code_link = code_url.code_url
+    ast = generate_ast_and_get_json(code_link)
     return {
-        "ast": generate_ast_and_get_json(code_link) 
+        "ast": ast,
+        "variables": find_variables_to_test(code_link) 
     }
 @app.post("/api/v1/code/project")
 async def generate_AST_from_project_url(code_url: ASTRequest):
     project_path = code_url.code_url #'./project-to-test/'
     asts = []
-
+    variables = []
     for root, _, filenames in os.walk(project_path):
         for filename in filenames:
             if filename.endswith('.c'):
                 filepath = os.path.join(root, filename)
                 ast = generate_ast_and_get_json(filepath)
                 asts.append(ast)
+                variables.append(find_variables_to_test(filepath))
+
     return {
         "numberOfAsts": len(asts),
-        "asts": asts 
+        "asts": asts,
+        "variables": variables 
     }
